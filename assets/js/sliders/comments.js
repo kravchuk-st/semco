5ecbb7a84dc651f85f239b31834ad2c412901c41
kravchuk-st <qkp@ukr.net>
2024-06-13 02:23:44 +0300

    add footer

---
+++ assets/js/sliders/comments.js
@@ -0,0 +1,8 @@
+new Splide( '.comments_slider', {
+  perMove: 1,
+  type   : 'loop',
+  perPage: 4,
+  autoplay: true,
+  gap: 40,
+  flickMaxPages: 1,
+} ).mount();
---
+++ assets/js/sliders/discount.js
@@ -0,0 +1,8 @@
+new Splide( '.discount_slider', {
+  perMove: 1,
+  type   : 'loop',
+  perPage: 3,
+  autoplay: true,
+  gap: 40,
+  flickMaxPages: 1,
+} ).mount();
